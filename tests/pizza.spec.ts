647f87fc5cb413b4dc7f2196ffb200fcba825b66
Sadie Crapo <scrapo5@byu.edu>
2026-02-12 21:57:47 -0700

    Test jwt verification

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -2,7 +2,7 @@ import { Page } from '@playwright/test';
 import { test, expect } from 'playwright-test-coverage';
 import { User, Role } from '../src/service/pizzaService';
 
-async function basicInit(page: Page) {
+async function basicInit(page: Page, valid: string = 'valid') {
     let loggedInUser: User | undefined;
     const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] }, 'a@jwt.com': { id: '2', name: 'Admin User', email: 'a@jwt.com', password: 'admin', roles: [{ role: Role.Admin }] } };
     const validFranchises = [{ id: 2, name: 'LotaPizza', stores: [{ id: 4, name: 'Lehi' }, { id: 5, name: 'Springville' }, { id: 6, name: 'American Fork' },], }, { id: 3, name: 'PizzaCorp', stores: [{ id: 7, name: 'Spanish Fork' }] }, { id: 4, name: 'topSpot', stores: [] }];
@@ -47,6 +47,13 @@ async function basicInit(page: Page) {
         expect(route.request().method()).toBe('GET');
         await route.fulfill({ json: loggedInUser });
     });
+
+    await page.route('*/**/api/order/verify', async (route) => {
+        expect(route.request().postDataJSON().jwt).toBe('eyJpYXQ');
+        // jwtMessage = valid ? 'valid' : 'invalid';
+        // valid = false;
+        await route.fulfill( { json: { message: valid, payload: {}}});
+    })
 
     // A standard menu
     await page.route('*/**/api/order/menu', async (route) => {
@@ -205,6 +212,12 @@ test('purchase with login', async ({ page }) => {
 
     // Check balance
     await expect(page.getByText('0.008')).toBeVisible();
+    await page.getByRole('button', { name: 'Verify' }).click();
+    await expect(page.locator('h3')).toContainText('JWT Pizza - valid');
+    await page.getByRole('button', { name: 'Close' }).click();
+    await expect(page.locator('h2')).toContainText('Here is your JWT Pizza!');
+});
+
 test('cancelled purchase', async ({ page }) => {
         await basicInit(page);
 
@@ -234,6 +247,41 @@ test('cancelled purchase', async ({ page }) => {
     await page.getByRole('button', { name: 'Cancel' }).click();
     await expect(page.locator('h2')).toContainText('Awesome is a click away');
 });
+
+test('purchase with invalid jwt', async ({ page }) => {
+    await basicInit(page);
+
+    // Go to order page
+    await page.getByRole('button', { name: 'Order now' }).click();
+
+    // Create order
+    await expect(page.locator('h2')).toContainText('Awesome is a click away');
+    await page.getByRole('combobox').selectOption('4');
+    await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+    await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+    await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    // Login
+    await page.getByPlaceholder('Email address').click();
+    await page.getByPlaceholder('Email address').fill('d@jwt.com');
+    await page.getByPlaceholder('Email address').press('Tab');
+    await page.getByPlaceholder('Password').fill('a');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Pay
+    await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+    await expect(page.locator('tbody')).toContainText('Veggie');
+    await expect(page.locator('tbody')).toContainText('Pepperoni');
+    await expect(page.locator('tfoot')).toContainText('0.008 ₿');
+    await page.getByRole('button', { name: 'Pay now' }).click();
+
+    // Check balance
+    await expect(page.getByText('0.008')).toBeVisible();
+    await page.getByRole('button', { name: 'Verify' }).click();
+    await expect(page.locator('h3')).toContainText('JWT Pizza - invalid');
+    await page.getByRole('button', { name: 'Close' }).click();
+    await expect(page.locator('h2')).toContainText('Here is your JWT Pizza!');
 });
 
 test('create franchise', async ({ page }) => {
